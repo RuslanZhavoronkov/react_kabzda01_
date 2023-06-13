de5ea0dbbb82e10b82eae1d44a51aa712f2c2be6
RuslanZhavoronkov <ruslanbear@mail.ru>
2023-06-13 08:54:31 +0300

    PageTitleType

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,11 @@ function App() {
   );
 }
 
-function PageTitle(props: any) {
+type PageTitlePropsType = {
+  title: string;
+};
+
+function PageTitle(props: PageTitlePropsType) {
   console.log("AppTitle rendering");
   return <h1>{props.title}</h1>;
 }
